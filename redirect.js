0a5853a9872e9870084df1c16bb7f51b502000ad
FLORAHAM989 <floraham@naver.com>
2025-02-12 13:32:33 +0900

    Create redirect.js

---
+++ redirect.js
@@ -0,0 +1,14 @@
+(function() {
+    const referrer = document.referrer || "Direct Visit";
+
+    // 🔹 블로그 URL인지 확인
+    if (referrer.includes("blog.naver.com")) {
+        console.log("블로그 URL 감지됨:", referrer);
+    } else {
+        console.log("블로그 URL 감지 실패, referrer:", referrer);
+    }
+
+    // 🔹 redirect.html로 이동하면서 블로그 URL을 넘김
+    const redirectUrl = "https://www.orozi.kr/redirect.html?blog_url=" + encodeURIComponent(referrer);
+    window.location.href = redirectUrl;
+})();
