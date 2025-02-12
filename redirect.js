51b54fe541c1af92a2ad0bbda56dec7811c10268
FLORAHAM989 <floraham@naver.com>
2025-02-12 14:26:23 +0900

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -8,8 +8,10 @@
         console.log("블로그 글 URL 감지 실패, referrer:", referrer);
     }
 
-    const redirectUrl = "https://www.orozi.kr/redirect?blog_url=" + encodeURIComponent(referrer);
+    // 사용자의 실제 redirect 페이지로 이동하면서 블로그 글 URL 전달
+    const redirectUrl = "https://floraham989.github.io/redirect-script/redirect.html?blog_url=" + encodeURIComponent(referrer);
 
+    // 확실한 리디렉션을 위해 setTimeout 사용
     setTimeout(() => {
         window.location.replace(redirectUrl);
     }, 500);
