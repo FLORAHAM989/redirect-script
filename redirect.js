e4f475d19f573692d03ed7fcd43092d80856d369
FLORAHAM989 <floraham@naver.com>
2025-02-12 14:10:33 +0900

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -1,11 +1,12 @@
 (function() {
     // 🔹 URL에서 블로그 글 주소 가져오기
     const urlParams = new URLSearchParams(window.location.search);
-    let referrer = urlParams.get("blog_url") || document.referrer || "Direct Visit";
+    let referrer = urlParams.get("blog_url") || document.referrer || sessionStorage.getItem("lastBlogUrl") || "Direct Visit";
 
     // 🔹 블로그 URL 감지
     if (referrer.includes("blog.naver.com")) {
         console.log("블로그 글 URL 감지됨:", referrer);
+        sessionStorage.setItem("lastBlogUrl", referrer); // 🔹 블로그 글 URL 저장
     } else {
         console.log("블로그 글 URL 감지 실패, referrer:", referrer);
     }
